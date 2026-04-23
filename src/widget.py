e342a434df052cd8c61dc606ec997bf3cd917e5c
Андрей Васильев <lr1975@yandex.ru>
2026-04-23 21:39:47 +0400

    добавлена функция get_date

--- src/widget.py
+++ src/widget.py
@@ -11,19 +11,29 @@ def mask_account_card(account_or_number: "str") -> str:
             number += ch
         else:
             premeditation += ch
+
     if len(number) == 16:
         return premeditation + get_mask_card_number(number)
     else:
         return premeditation + get_mask_account(number)
 
 
-aaa = ['Maestro 1596837868705199',
-       'Счет 64686473678894779589',
-       'MasterCard 7158300734726758',
-       'Счет 35383033474447895560',
-       'Visa Classic 6831982476737658',
-       'Visa Platinum 8990922113665229',
-       'Visa Gold 5999414228426353',
-       'Счет 73654108430135874305']
-for i in aaa:
-    print(mask_account_card(i))
+def get_date(data_time: str) -> str:
+    """ принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+        и возвращает строку с датой в формате "ДД.ММ.ГГГГ  ("11.03.2024")"""
+
+    return data_time[8:10] + '.' + data_time[5:7] + '.' + data_time[:4]
+
+
+print(get_date('2024-03-11T02:26:18.671407'))
+
+# aaa = ['Maestro 1596837868705199',
+#        'Счет 64686473678894779589',
+#        'MasterCard 7158300734726758',
+#        'Счет 35383033474447895560',
+#        'Visa Classic 6831982476737658',
+#        'Visa Platinum 8990922113665229',
+#        'Visa Gold 5999414228426353',
+#        'Счет 73654108430135874305']
+# for i in aaa:
+#     print(mask_account_card(i))
