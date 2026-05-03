1c7a7acc7b321c1e64149bfd288609974a5d7bda
Андрей Васильев <lr1975@yandex.ru>
2026-05-03 08:03:28 +0400

    подправлено по результатам проверки

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
 
-def filter_by_state(list_dict: list[dict], state='EXECUTED') -> list[dict]:
+def filter_by_state(list_dict: list[dict], state="EXECUTED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-        'state' соответствует указанному значению."""
+    'state' соответствует указанному значению."""
 
-    return list(dict_ for dict_ in list_dict if dict_['state'] == state)
+    return list(dict_ for dict_ in list_dict if dict_["state"] == state)
 
 
 def sort_by_date(list_dicts: list[dict], ascending=True) -> list[dict]:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате"""
 
-    return sorted(list_dicts, key=lambda x: datetime.fromisoformat(x['date']), reverse=ascending)
+    return sorted(list_dicts, key=lambda x: datetime.fromisoformat(x["date"]), reverse=ascending)
--- src/widget.py
+++ src/widget.py
@@ -4,11 +4,11 @@ from src.masks import get_mask_account
 from src.masks import get_mask_card_number
 
 
-def mask_account_card(account_or_number: "str") -> str:
+def mask_account_card(account_or_number: str) -> str:
     """Обрабатывает информацию как о картах, так и о счетах"""
 
-    number = ''.join([ch for ch in account_or_number if ch.isdigit()])
-    text = ''.join([ch for ch in account_or_number if not ch.isdigit()])
+    number = "".join([ch for ch in account_or_number if ch.isdigit()])
+    text = "".join([ch for ch in account_or_number if not ch.isdigit()])
 
     return text + get_mask_card_number(number) if len(number) == 16 else text + get_mask_account(number)
 
@@ -17,4 +17,4 @@ def get_date(data_time: str) -> str:
     """принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ ("11.03.2024")"""
 
-    return datetime.fromisoformat(data_time).strftime("%d-%m-%Y").replace('-', '.')
+    return datetime.fromisoformat(data_time).strftime("%d-%m-%Y").replace("-", ".")
