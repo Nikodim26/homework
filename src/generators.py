9cedc0d2aef50596d4c02ad972dc543f86c5b0ca
Андрей Васильев <lr1975@yandex.ru>
2026-05-11 10:49:22 +0400

    создана transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -6,5 +6,10 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Generator[dic
     Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует
     заданной (например, USD)."""
 
-
     return (i for i in transactions if i["operationAmount"]["currency"]["code"] == currency)
+
+
+def transaction_descriptions(transactions: list[dict]) -> Generator[dict, Any, None]:
+    """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
+
+    return (i["description"] for i in transactions)
