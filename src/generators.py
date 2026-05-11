65a8aa73ba0ca0d2a964ab18f52a10aff15b452c
Андрей Васильев <lr1975@yandex.ru>
2026-05-11 11:32:55 +0400

    создана card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -13,3 +13,17 @@ def transaction_descriptions(transactions: list[dict]) -> Generator[dict, Any, N
     """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
 
     return (i["description"] for i in transactions)
+
+
+def card_number_generator(start_of_range: int, end_of_range: int) -> Generator[str]:
+    """Выдает номера банковских карт в формате "XXXX XXXX XXXX XXXX", где X — цифра номера карты.
+    Может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999"""
+
+    for i in range(start_of_range, end_of_range + 1):
+        card_number = ''
+        for j in range(17 - len(str(end_of_range))):
+            card_number += '0'
+
+        card_number += str(i)
+
+        yield card_number[:4] + " " + card_number[4:8] + " " + card_number[8:12] + " " + card_number[-4:]
