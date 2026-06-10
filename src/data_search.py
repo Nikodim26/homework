ecef0392421fece67bc546ce9fb41b1ba9345c9a
Андрей Васильев <lr1975@yandex.ru>
2026-06-10 15:32:27 +0400

    а была не была

--- src/data_search.py
+++ src/data_search.py
@@ -1,4 +1,4 @@
-def data_search(data: str) -> list[dict]:
+def process_bank_search(data: list[dict], search_string: str) -> list[dict]:
     """Ищет в данных нужные, обусловленные определенными критериями"""
 
-    return []
+    return [dict_data for dict_data in data if search_string in str(dict_data)]
