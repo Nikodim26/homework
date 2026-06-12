96038fcc566a8b7be213e6402b0ba4569bc4a765
Андрей Васильев <lr1975@yandex.ru>
2026-06-12 18:05:08 +0400

    изменил process_bank_operations с Counter

--- src/data_search.py
+++ src/data_search.py
@@ -1,10 +1,11 @@
 import json
 import re
+from collections import Counter
 
 
 def process_bank_search(data: list[dict], search_string: str) -> list[dict]:
     """Ищет в данных нужные, обусловленные определенными критериями"""
-    res=[]
+    res = []
     for i in data:
         data_json = re.sub(r"[\"()\[\]{}]", "", json.dumps(i, ensure_ascii=False))
         if search_string.lower() in data_json.lower():
@@ -14,10 +15,10 @@ def process_bank_search(data: list[dict], search_string: str) -> list[dict]:
 
 def process_bank_operations(data: list[dict], categories: list) -> dict:
     """Собирает статистику об операциях - их количестве"""
-    descriptions = set(dict_data.get("description") for dict_data in data if dict_data.get("description"))
-    result = {}
-    for description in descriptions:
-        if description in categories:
-            result[description] = len(process_bank_search(data, description))
+    types_of_categories = []
+    for categori in categories:
+        category_matching_dictionary = process_bank_search(data, categori)
+        for category_matching in category_matching_dictionary:
+            types_of_categories.append(categori)
 
-    return result
+    return dict(Counter(types_of_categories))
