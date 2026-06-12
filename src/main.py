06262f2329a24275efb2bd8a081d0fe86a09a605
Андрей Васильев <lr1975@yandex.ru>
2026-06-12 16:37:08 +0400

    подправил и дополнил main

--- src/main.py
+++ src/main.py
@@ -23,61 +23,81 @@ def main() -> None:
     """)
     while True:
         number = input("Введите номер пункта: ")
-
         if not number.isdigit():
             continue
-        number = int(number)
-        if number in [1, 2, 3]:
+        elif number in ['1', '2', '3']:
+            print(f'Для обработки выбран "{['JSON', 'CSV', 'XLSX'][int(number) - 1]}"-файл.')
             break
-    print(f'Для обработки выбран "{['JSON', 'CSV', 'XLSX'][number - 1]}"-файл.')
 
-    if number == 1:
-        transactions = about_financial_transactions_json("operations.json")
-    if number == 2:
-        transactions = about_financial_transactions_csv("transactions.csv")
-    if number == 3:
-        transactions = about_financial_transactions_xlsx("transactions_excel.xlsx")
+    match number:
+        case '1':
+            transactions = about_financial_transactions_json("operations.json")
+        case '2':
+            transactions = about_financial_transactions_csv("transactions.csv")
+        case '3':
+            transactions = about_financial_transactions_xlsx("transactions_excel.xlsx")
 
     while True:
         status_val = input("""
     Введите статус, по которому необходимо выполнить фильтрацию.
     Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING
-    (по умолчанию 'EXECUTED'): """).upper()
+    /'EXECUTED'): """).upper()
 
-        if not status_val:
+        if status_val in ["EXECUTED", "CANCELED", "PENDING"]:
+            break
+        if status_val == "":
             status_val = "EXECUTED"
             break
-        if status_val not in ["EXECUTED", "CANCELED", "PENDING"]:
-            print(f"Статус операции {status_val} недоступен")
-        else:
-            break
-    print(f'Операции отфильтрованы по статусу "{status_val}"')
+        print(f"Статус операции {status_val} недоступен")
+
     transactions = filter_by_state(transactions, status_val)
+    print(f'Операции отфильтрованы по статусу "{status_val}"')
 
     while True:
-        answer_to_question_sort = input("Отсортировать операции по дате (Да/Нет) ? ").lower()
+        answer_to_question_sort = input("Отсортировать операции по дате (Да/Нет) /Да ? ").lower()
         if answer_to_question_sort in ["да", "нет"]:
             break
-
-    answer_to_question_sort = "да"
-
-    while True:
-        answer_to_question_sort_type = input("Отсортировать по возрастанию или по убыванию (возр/убыв) ? ").lower()
-        if answer_to_question_sort_type in ["возр", "убыв"]:
-            ascending = True if answer_to_question_sort_type == "убыв" else False
+        if answer_to_question_sort == '':
+            answer_to_question_sort = 'да'
             break
 
-    if answer_to_question_sort == "да":
+    while True:
+        answer_to_question_sort_type = input(
+            "Отсортировать по возрастанию или по убыванию (возр/убыв) /убыв ? ").lower()
+        if answer_to_question_sort_type in ["возр", "убыв"]:
+            break
+        if answer_to_question_sort_type == "":
+            answer_to_question_sort_type = "убыв"
+            break
+
+    ascending = True if answer_to_question_sort_type == 'убыв' else False
+    if answer_to_question_sort == 'да':
         transactions = sort_by_date(transactions, ascending)
 
     while True:
-        answer_to_question_sort_val = input("Выводить только рублевые транзакции (Да/Нет) ? ").lower()
+        answer_to_question_sort_val = input("Выводить только рублевые транзакции (Да/Нет) /да ? ").lower()
         if answer_to_question_sort_val in ["да", "нет"]:
-            is_rub = True if answer_to_question_sort_val == "да" else False
+            break
+        if answer_to_question_sort_val == "":
+            answer_to_question_sort_val = 'да'
             break
 
-    if is_rub:
+    if answer_to_question_sort_val == 'да':
         transactions = process_bank_search(transactions, "RUB")
+
+
+    while True:
+        answer_to_question_sort_word = input(
+            'Отфильтровать список транзакций по определенному слову в описании Да/Нет ? /Да')
+        if answer_to_question_sort_word in ["да", "нет"]:
+            break
+        if answer_to_question_sort_word == "":
+            answer_to_question_sort_word = 'да'
+            break
+
+    if answer_to_question_sort_word == 'да':
+        word_for_sort = input('Введите слово для сортировки ')
+        transactions = process_bank_search(transactions,word_for_sort)
 
     print("Распечатываю итоговый список транзакций...")
     print(f"Всего банковских операций в выборке: {len(transactions)}")
